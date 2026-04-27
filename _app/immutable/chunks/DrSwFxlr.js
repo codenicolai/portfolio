526d089950413a64917a48c24aa9d1537667becb
nicolai <nicolaileo@outlook.com>
2026-04-27 19:07:13 -0300

    Updates

--- _app/immutable/chunks/DrSwFxlr.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1otbuyi?.base??"/portfolio",t=globalThis.__sveltekit_1otbuyi?.assets??s??"";export{t as a,s as b};
---
+++ _app/immutable/entry/start.CoVaELCa.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/i5BZPDG8.js";export{o as load_css,r as start};
